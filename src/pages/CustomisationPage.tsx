bac7e3d3fd82877d053ddeba93596612450e6fa7
raphpay <raphpay.dev@gmail.com>
2025-08-08 16:50:34 +0400

    Handle image on image #5

--- src/pages/CustomisationPage.tsx
+++ src/pages/CustomisationPage.tsx
@@ -1,7 +1,36 @@
-import React from "react";
+import { Image as KonvaImage, Layer, Stage } from "react-konva";
+import useImage from "use-image";
+import MousePadImage from "../assets/mouse-pad.png";
 
-const CustomisationPage: React.FC = () => {
-  return <div className="max-w-md mx-auto p-4">Custom</div>;
-};
+function CustomisationPage() {
+  const customerImageUrl = import.meta.env.VITE_CUSTOM_TEST_URL;
+  const [mousePadImage] = useImage(MousePadImage);
+  const [clientImage] = useImage(customerImageUrl);
+
+  return (
+    <Stage width={500} height={300}>
+      <Layer>
+        {/* Arrière-plan = tapis */}
+        <KonvaImage
+          image={mousePadImage}
+          x={0}
+          y={0}
+          width={500}
+          height={300}
+        />
+
+        {/* Image client par-dessus */}
+        <KonvaImage
+          image={clientImage}
+          x={150}
+          y={80}
+          width={200}
+          height={140}
+          draggable
+        />
+      </Layer>
+    </Stage>
+  );
+}
 
 export default CustomisationPage;
