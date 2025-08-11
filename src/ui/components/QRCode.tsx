dfe16073a901fed9a2080fc356f5ca26fba5418f
raphpay <raphpay.dev@gmail.com>
2025-08-11 15:53:15 +0400

    Simplify QRcode

--- src/ui/components/QRCode.tsx
+++ src/ui/components/QRCode.tsx
@@ -1,5 +1,4 @@
 import { QRCodeSVG } from "qrcode.react";
-import Logo from "../../assets/tof-bike.png";
 
 interface QRCodeProps {
   value: string;
@@ -9,19 +8,6 @@ function QRCode({ value }: QRCodeProps) {
   return (
     <div style={{ position: "relative", display: "inline-block" }}>
       <QRCodeSVG value={value} size={200} />
-      <img
-        src={Logo}
-        alt="logo"
-        style={{
-          position: "absolute",
-          top: "50%",
-          left: "50%",
-          width: 40,
-          height: 40,
-          transform: "translate(-50%, -50%)",
-          borderRadius: "8px", // optional
-        }}
-      />
     </div>
   );
 }
